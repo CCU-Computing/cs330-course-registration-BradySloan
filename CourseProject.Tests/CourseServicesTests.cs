d2c8abbd24a09b8fb7999439dbac12b29afb6693
BradySloan <bcsloan@coastal.edu>
2026-04-16 15:51:35 -0400

    added .scproj and tests file

---
+++ CourseProject.Tests/CourseServicesTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using cs330_proj1;
+using System.Collections.Generic;
+using Moq;
+using System.Linq;
+
+
+namespace CourseProject.Tests
+{
+    public class CourseServicesTests
+    {
+
+    }
+}
