ce28cc030fcfdb86d0f78a1d3dbfac8c3624d7ae
BradySloan <bcsloan@coastal.edu>
2026-04-17 14:53:50 -0400

    added unit tests for 2

--- CourseProject.Tests/CourseServicesTests.cs
+++ CourseProject.Tests/CourseServicesTests.cs
@@ -94,7 +94,38 @@ namespace CourseProject.Tests
         //Add unit tests for GetOfferingsByGoalIdAndSemester_GoalIsFoundAndMultipleCourseOfferingsAreInSemester_OfferingsAreReturned()
         // Add unit test for GetOfferingsByGoalIdAndSemester_GoalIsFoundAndNoCourseOfferingIsInSemester_EmptyListIsReturned()
 
+        [Fact]
+        public void GetCourses_ReturnsAllCourses()
+        {
+            var mockRepository = new Mock<ICourseRepository>();
+            var courses = GetTestCourses();
 
+            mockRepository.Setup(m => m.Courses).Returns(courses);
+            mockRepository.Setup(m => m.Goals).Returns(new List<CoreGoal>());
+            mockRepository.Setup(m => m.Offerings).Returns(new List<CourseOffering>());
+
+            var service = new CourseServices(mockRepository.Object);
+
+            var result = service.getCourses();
+
+            Assert.Equal(courses.Count, result.Count);
+        }
+
+        [Fact]
+        public void GetCourses_NoCourses_ReturnsEmptyList()
+        {
+            var mockRepository = new Mock<ICourseRepository>();
+
+            mockRepository.Setup(m => m.Courses).Returns(new List<Course>());
+            mockRepository.Setup(m => m.Goals).Returns(new List<CoreGoal>());
+            mockRepository.Setup(m => m.Offerings).Returns(new List<CourseOffering>());
+
+            var service = new CourseServices(mockRepository.Object);
+
+            var result = service.getCourses();
+
+            Assert.Empty(result);
+        }
         private List<Course> GetTestCourses()
         {
             return new List<Course>(){
