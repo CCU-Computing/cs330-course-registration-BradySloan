d70409171b9bb504dfbc83d8f3afd61055153158
BradySloan <bcsloan@coastal.edu>
2026-04-17 14:56:12 -0400

    added unit tests for 3

--- CourseProject.Tests/CourseServicesTests.cs
+++ CourseProject.Tests/CourseServicesTests.cs
@@ -126,6 +126,41 @@ namespace CourseProject.Tests
 
             Assert.Empty(result);
         }
+        [Fact]
+        public void GetCourseOfferingsBySemester_OneMatch_ReturnsOffering()
+        {
+            var course = GetTestCourses().First();
+
+            var offerings = new List<CourseOffering>() {
+                new CourseOffering {
+                    Semester = "Fall 2020",
+                    Section = "1",
+                    TheCourse = course
+                }
+            };
+
+            var mockRepository = new Mock<ICourseRepository>();
+            mockRepository.Setup(m => m.Offerings).Returns(offerings);
+
+            var service = new CourseServices(mockRepository.Object);
+
+            var result = service.getCourseOfferingsBySemester("Fall 2020");
+
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public void GetCourseOfferingsBySemester_NoMatch_ReturnsEmpty()
+        {
+            var mockRepository = new Mock<ICourseRepository>();
+            mockRepository.Setup(m => m.Offerings).Returns(new List<CourseOffering>());
+
+            var service = new CourseServices(mockRepository.Object);
+
+            var result = service.getCourseOfferingsBySemester("Spring 2025");
+
+            Assert.Empty(result);
+        }
         private List<Course> GetTestCourses()
         {
             return new List<Course>(){
